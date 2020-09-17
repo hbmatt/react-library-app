928821abaf25953a1bd7bdce0dcac281011f51f2
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-09-17 15:46:43 -0500

    Updates

---
+++ precache-manifest.d855724438149495dba700e7a0fe43df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae19916a069537b0cbb597c10aab767",
+    "url": "/react-library-app/index.html"
+  },
+  {
+    "revision": "3a65315801a0221e1f0e",
+    "url": "/react-library-app/static/css/main.894a2778.chunk.css"
+  },
+  {
+    "revision": "98528ec3b3fbb67a36fa",
+    "url": "/react-library-app/static/js/2.543a8269.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-library-app/static/js/2.543a8269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a65315801a0221e1f0e",
+    "url": "/react-library-app/static/js/main.9ce2dc46.chunk.js"
+  },
+  {
+    "revision": "d8325cbfaba466a4d69e",
+    "url": "/react-library-app/static/js/runtime-main.7db85d2d.js"
+  }
+]);
